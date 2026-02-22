1ebad2562a1afe790ee546341c1fa5410288a0ec
Waden Intader <wadengintander@gmail.com>
2026-02-22 19:24:21 +0800

    Update hero-branding.tsx

--- src/components/sections/hero-branding.tsx
+++ src/components/sections/hero-branding.tsx
@@ -64,7 +64,7 @@ const HeroBranding = () => {
           {/* Main Headline */}
         <div className="flex flex-col items-center gap-0.5 px-2">
         <div className="relative">
-            <h1 className="text-[21px] md:text-[23.5px] font-extrabold leading-tight tracking-tight flex items-center justify-center gap-x-2 flex-wrap font-poppins">
+            <h1 className="text-[19px] md:text-[23.5px] font-extrabold leading-tight tracking-tight flex items-center justify-center gap-x-2 flex-wrap font-poppins">
             <span className="text-[#182C54]">Unlock</span>
             <span className="text-[#006241]">Starbucks Rewards</span>
             <div className="flex items-center -ml-1">
@@ -87,3 +87,4 @@ const HeroBranding = () => {
 };
 
 export default HeroBranding;
+
